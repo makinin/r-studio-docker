005dff84e4d24bd1462e158fd265e08c9f3fcb60
makinin <marija.ninic@nsd.no>
2021-03-31 19:12:01 +0200

    Create rpackages.R

---
+++ rscript/rpackages.R
@@ -0,0 +1,7 @@
+# code to install packages in the docker file
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("magrittr")
+install.packages("highcharter")
+install.packages("openxlsx")
+install.packages("stringr")
